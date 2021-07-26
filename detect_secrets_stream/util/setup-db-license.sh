222ed8ff0a5f4e96716f78fe8e54ef2fd3cb2caf
killuazhu <xianjun@us.ibm.com>
2021-07-26 15:48:42 -0400

    No longer need test (#34)

--- detect_secrets_stream/util/setup-db-license.sh
+++ detect_secrets_stream/util/setup-db-license.sh
@@ -6,7 +6,7 @@ PIP_DIR=$(pip show ibm-db | grep Location: | awk '{print $2}')
 DB2_LICENSE_FILE=${PIP_DIR}/clidriver/license/db2consv_zs.lic
 : ${DB2_SECRET_FILE:=/gd-secret/db2consv_zs.lic}
 
-if [ test -s "${DB2_SECRET_FILE}" ]; then
+if [ -s "${DB2_SECRET_FILE}" ]; then
   cp "${DB2_SECRET_FILE}" "${DB2_LICENSE_FILE}"
 else
   echo "${DB2_SECRET_FILE} does not exist or is empty."
